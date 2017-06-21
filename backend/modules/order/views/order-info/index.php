e7b7804a9c1127dc1c7a14fb57d4b5df0600b0a9
sunsweet <sunsweet@foxmail.com>
2017-06-21 13:37:14 +0800

    bug fix

--- backend/modules/order/views/order-info/index.php
+++ backend/modules/order/views/order-info/index.php
@@ -24,6 +24,10 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <?php
     echo  GridView::widget([
+        'tableOptions' => [
+            'style' => 'display:block;overflow:auto;',
+            'class' => 'table table-striped table-bordered'
+        ],
         'dataProvider' => $dataProvider,
         'rowOptions' => function($model, $key, $index, $grid) {
             //有售后的显示红色
@@ -44,7 +48,7 @@ $this->params['breadcrumbs'][] = $this->title;
             [
                 'header'=>'查看',
                 'class' => 'yii\grid\ActionColumn',
-                'headerOptions' => ['width' => '15'],
+                'headerOptions' => ['style' => 'min-width:50px'],
                 'template' => '{open}',
                 'buttons' => [
                     'open' => function ($url, $model, $key) {
@@ -54,7 +58,7 @@ $this->params['breadcrumbs'][] = $this->title;
             [
                 'header'=>'操作',
                 'class' => 'yii\grid\ActionColumn',
-                'headerOptions' => ['width' => '75'],
+                'headerOptions' => ['style' => 'min-width:70px'],
                 'template' => '{view} {add} {delivery}{delivery-direct}{delivery-after}',
                 'buttons' =>[
                     'add' => function ($url, $model, $key) {
@@ -118,48 +122,70 @@ $this->params['breadcrumbs'][] = $this->title;
 
             [
                 'attribute' => 'id',
-                'headerOptions' => ['width' => '75'],
+                'headerOptions' => ['style' => 'min-width:50px'],
             ],
-            'order_sn',
-            'create_time',
+            [
+                'headerOptions' => ['style' => 'min-width:100px'],
+                'attribute'=>'order_sn'
+            ],
+            [
+                'attribute' => 'create_time',
+                'headerOptions' => ['style' => 'min-width:100px']
+            ],
             [
                 'attribute' => 'user_id',
                 'value' => 'c_user.user_name',
-                'label'=>'会员'
+                'label'=>'会员',
+                'headerOptions' => ['style' => 'min-width:100px']
             ],
             [
                 'attribute' => 'order_object_id',
                 'value' => 'order_object.order_sn',
-                'label'=>'父订单号'
+                'label'=>'父订单号',
+                'headerOptions' => ['style' => 'min-width:100px']
             ],
-            'store_name',
             [
+                'headerOptions' => ['style' => 'min-width:100px'],
+                'attribute' => 'store_name',
+            ],
+            [
+                'headerOptions' => ['style' => 'min-width:100px'],
                 'attribute' => 'refund',
                 'value' => function ($model) {
                     return OrderInfo::dropDown('refund', $model->refund);
                 }
             ],
             [
+                'headerOptions' => ['style' => 'min-width:100px'],
                 'attribute' => 'pay_type',
                 'value' => function ($model) {
                     return OrderInfo::dropDown('pay_type', $model->pay_type);
                 }
             ],
-            'link_man',
-            'mobile',
             [
+                'attribute' => 'link_man',
+                'headerOptions' => ['style' => 'min-width:100px'],
+            ],
+            [
+                'attribute' => 'mobile',
+                'headerOptions' => ['style' => 'min-width:100px'],
+            ],
+            [
+                'headerOptions' => ['style' => 'min-width:100px'],
                 'attribute' => 'total_price',
                 'value' => function ($model) {
                     return $model->total_price/100;
                 }
             ],
             [
+                'headerOptions' => ['style' => 'min-width:100px'],
                 'attribute' => 'delivery_way',
                 'value' => function ($model) {
                     return OrderInfo::dropDown('delivery_way', $model->delivery_way);
                 },
             ],
             [
+                'headerOptions' => ['style' => 'min-width:50px'],
                 'attribute' => 'procedure_fee',
                 'label'=>'运费',
                 'value' => function ($model) {
@@ -168,6 +194,7 @@ $this->params['breadcrumbs'][] = $this->title;
             ],
 
             [
+                'headerOptions' => ['style' => 'min-width:100px'],
                 'attribute' => 'status',
                 'value' => function ($model) {
                     return OrderInfo::dropDown('status', $model->status);

cb9bf947d6efb47bd8ceece46d7fe3abc7aae20a
youweiqi <360063842@qq.com>
2017-08-25 10:20:16 +0800

    2222222

--- backend/web/test.php
+++ backend/web/test.php
@@ -7,4 +7,10 @@
  */
 
 
- 
+
+
+    $h_code = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X'];
+    $goods_bn =$h_code[intval(date('H'))] . strtoupper(dechex(date('m'))) . date('d') . substr(time(), -5) . substr(microtime(), 2, 4) . sprintf('%02d', rand(0, 99));
+    echo  $goods_bn."</br>";
+    echo  $a =  substr(time(), -5);
+
